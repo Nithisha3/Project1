67725cab09e20c143e9fd2329dbf8d288d0444da
vyshnavi0409 <59179834+vyshnavi0409@users.noreply.github.com>
2020-11-19 19:45:06 +0530

    Update Claim.java

--- OnlineClaim/src/main/java/cg/ocrs/dao/Claim.java
+++ OnlineClaim/src/main/java/cg/ocrs/dao/Claim.java
@@ -2,50 +2,55 @@ package repositry;
 
 import java.sql.*;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cg.model.ClaimAdjuster;
+import com.cg.model.Claimlist;
+import com.cg.model.Userrole;
 
 public class Claim implements IClaim{
 	Connection con;
 	PreparedStatement psmt;
 	ResultSet rsUser;
+	Claimlist clist=new Claimlist();
 	public Claim() {
-		try {
-			con=Connectionutil.getConnection();
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+	try {
+		con=Connectionutil.getConnection();
+	} 
+	catch (Exception e) {
+		e.printStackTrace();
 	}
+}
 	@Override
 	public ClaimAdjuster addClaim(ClaimAdjuster cl) throws SQLException {
 		// TODO Auto-generated method stub
-		psmt=con.prepareStatement("insert into Claim values(?,?,?,?,?,?,?,?,?)");
+		psmt=con.prepareStatement("insert into claim values(?,?,?,?,?,?,?)");
 		psmt.setInt(1,cl.getClaimId());
 		psmt.setString(2,cl.getClaimReason());
 		psmt.setString(3, cl.getClaimLocation());
 		psmt.setString(4,cl.getClaimState());
 		psmt.setString(5, cl.getClaimCity());
 		psmt.setInt(6, cl.getClaimZip());
-		psmt.setString(7,cl.getClaimList().getClaimType());
-		psmt.setInt(8,cl.getClaimList().getPolicyNumber());
-		psmt.setInt(9, cl.getClaimList().getClaimNumber());
+		psmt.setString(7, cl.getClaimType());
+        psmt.executeUpdate();
 		return cl;
 	}
 	@Override
-	public ClaimAdjuster getClaim(int claimId) throws SQLException {
-		psmt=con.prepareStatement("select * from claim where id=?");
+	public ClaimAdjuster getClaim(int claimId) throws SQLException{
+		psmt=con.prepareStatement("select Location,City from claim where claimId=?");
 		psmt.setInt(1, claimId);
 		rsUser=psmt.executeQuery();
 		ClaimAdjuster ca=new ClaimAdjuster();
-		ca.setClaimId(rsUser.getInt("claimId"));
-		ca.setClaimReason(rsUser.getString("claimReason"));
+		while(rsUser.next()) {
+		ca.setClaimReason(rsUser.getString("claimReason"));	
 		ca.setClaimLocation(rsUser.getString("Location"));
-		ca.setClaimCity(rsUser.getString("City"));
 		ca.setClaimState(rsUser.getString("State"));
+		ca.setClaimCity(rsUser.getString("City"));
 		ca.setClaimZip(rsUser.getInt("Zip"));
-		// TODO Auto-generated method stub
-		return ca;
+		ca.setClaimType(rsUser.getString("claimType"));
+		
+	}
+			return ca;
 	}
 }
