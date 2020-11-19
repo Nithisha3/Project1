bce5ee1e7c68901fd65f335ddd5dc8b04eaf021f
sushmapulluri <psushmasdpt@gmail.com>
2020-11-19 10:28:43 +0530

    commit

--- src/main/java/cg/ocrs/dao/IPolicyDetails.java
+++ src/main/java/cg/ocrs/dao/IPolicyDetails.java
@@ -1,5 +1,15 @@
 package cg.ocrs.dao;
 
-public interface IPolicyDetails {
+import java.sql.SQLException;
 
-}
+import antlr.collections.List;
+import cg.ocrs.model.PolicyDetails;
+
+public interface IPolicyDetails {
+		public List getAllPolicyDetails() throws SQLException;
+		public PolicyDetails addPolicyDetails(PolicyDetails policyDetails) throws SQLException;
+
+	}
+
+
+
