82440f32c68e3b9e9fd81b9cba1b66fa7f54607d
vyshnavi0409 <59179834+vyshnavi0409@users.noreply.github.com>
2020-11-24 12:41:41 +0530

    Update IClaim.java

--- OnlineClaim/src/main/java/cg/ocrs/dao/IClaim.java
+++ OnlineClaim/src/main/java/cg/ocrs/dao/IClaim.java
@@ -7,7 +7,8 @@ import com.cg.model.ClaimAdjuster;
 
 public interface IClaim {
 		public ClaimAdjuster addClaim(ClaimAdjuster claimList)throws SQLException;
-		public ClaimAdjuster getClaim(int claimId)throws SQLException;
-		
+		public ClaimAdjuster getClaim(String claimId)throws SQLException;
+		public ArrayList<QuestionAndAnswers> getQuestionAndAnswers(int questionId)throws SQLException;
+		public int getQuestionName(String questionName) throws SQLException;
 
 	}
